07bb53a6df2b96817a3d1303ef93bfb2cd849f05
Binod Shrestha <shresthabinod.softdev@gmail.com>
2021-04-18 13:07:41 -0400

    practices with java stream

--- src/com/shresthabinod/functionalProgramming/Main.java
+++ src/com/shresthabinod/functionalProgramming/Main.java
@@ -1,5 +1,9 @@
 package com.shresthabinod.functionalProgramming;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         StudentInterface studentInterface = () -> System.out.println("My grade is: A+");
@@ -12,5 +16,19 @@ public class Main {
         System.out.println(calculation.plus(10, 20));
         var minus = calculation.minusNum(50,20);
         System.out.println(minus);
+
+        // stream in java
+        List<Student> studentList = Arrays.asList(
+                new Student("Binod", "6 Berringer Grove", 35, "Computer Science", 2021),
+                new Student("Puja", "6 Berringer Grove", 30, "Beauty", 2021),
+                new Student("Punjan", "8 Berringer Grove", 20, "Music", 2021),
+                new Student("Aayush", "6 Berringer Grove", 11, "Automation", 2021));
+        var newStudentList = studentList.stream()
+                .filter(student -> student != null) // not null
+                .filter(student -> student.graduation == 2021) // get all who are graduating in 2021
+                .filter(student -> student.name.startsWith("P") && student.age == 20) // list of students whose name start with "P" and age is 20
+                .collect(Collectors.toList()); // store all the filtered students as list
+        System.out.println(newStudentList); // print the list
+
     }
 }
--- src/com/shresthabinod/functionalProgramming/Student.java
+++ src/com/shresthabinod/functionalProgramming/Student.java
@@ -1,4 +1,68 @@
 package com.shresthabinod.functionalProgramming;
 
 public class Student {
+String name;
+String address;
+int age;
+String program;
+int graduation;
+
+    public Student(String name, String address, int age, String program, int graduation) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.program = program;
+        this.graduation = graduation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public int getGraduation() {
+        return graduation;
+    }
+
+    public void setGraduation(int graduation) {
+        this.graduation = graduation;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                ", program='" + program + '\'' +
+                ", graduation=" + graduation +
+                '}';
+    }
 }
